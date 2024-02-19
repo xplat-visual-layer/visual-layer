97f91bb3771daf8878674d7f32fc3d4c2b7f4b67
Bret Johnson <bret.johnson@microsoft.com>
2024-02-19 15:38:48 -0500

    Update Visual Composition -> Visual Layer in messages

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Visual Composition Project. All rights reserved.
+// Copyright (c) Visual Layer Project. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Runtime.InteropServices;
