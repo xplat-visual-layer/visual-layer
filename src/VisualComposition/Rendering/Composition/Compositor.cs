53b145d057dabc06a341e0b73a27b75e06e2b325
Bret Johnson <bret.johnson@microsoft.com>
2024-02-19 01:24:31 -0500

    Add Rendering directory, frpm Avalonia
    Copy the Rendering directory from Avalonia, as of this commit:
    https://github.com/AvaloniaUI/Avalonia/tree/073a530ed699fda3583316268b1ea7e46e765bcc
    
    This copy has no changes at all - we'll update from here.

---
+++ src/VisualComposition/Rendering/Composition/Compositor.cs
@@ -0,0 +1,292 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Avalonia.Animation;
+using Avalonia.Animation.Easings;
+using Avalonia.Media;
+using Avalonia.Metadata;
+using Avalonia.Platform;
+using Avalonia.Rendering.Composition.Server;
+using Avalonia.Rendering.Composition.Transport;
+using Avalonia.Threading;
+using Avalonia.Utilities;
+
+namespace Avalonia.Rendering.Composition
+{
+    /// <summary>
+    /// The Compositor class manages communication between UI-thread and render-thread parts of the composition engine.
+    /// It also serves as a factory to create UI-thread parts of various composition objects 
+    /// </summary>
+    public partial class Compositor
+    {
+        internal IRenderLoop Loop { get; }
+        internal bool UseUiThreadForSynchronousCommits { get; }
+        private readonly ServerCompositor _server;
+        private CompositionBatch? _nextCommit;
+        private readonly BatchStreamObjectPool<object?> _batchObjectPool;
+        private readonly BatchStreamMemoryPool _batchMemoryPool;
+        private readonly Queue<ICompositorSerializable> _objectSerializationQueue = new();
+        private readonly HashSet<ICompositorSerializable> _objectSerializationHashSet = new();
+        private Queue<Action> _invokeBeforeCommitWrite = new(), _invokeBeforeCommitRead = new();
+        private readonly HashSet<IDisposable> _disposeOnNextBatch = new();
+        internal ServerCompositor Server => _server;
+        private CompositionBatch? _pendingBatch;
+        private readonly object _pendingBatchLock = new();
+        private readonly List<Action> _pendingServerCompositorJobs = new();
+        private DiagnosticTextRenderer? _diagnosticTextRenderer;
+        private readonly Action _triggerCommitRequested;
+
+        internal IEasing DefaultEasing { get; }
+
+        internal Dispatcher Dispatcher { get; }
+
+        private DiagnosticTextRenderer? DiagnosticTextRenderer
+        {
+            get
+            {
+                if (_diagnosticTextRenderer == null)
+                {
+                    // We are running in some unit test context
+                    if (AvaloniaLocator.Current.GetService<IFontManagerImpl>() == null)
+                        return null;
+                    _diagnosticTextRenderer = new(Typeface.Default.GlyphTypeface, 12.0);
+                }
+
+                return _diagnosticTextRenderer;
+            }
+        }
+
+        internal event Action? AfterCommit;
+
+        
+        /// <summary>
+        /// Creates a new compositor on a specified render loop that would use a particular GPU
+        /// </summary>
+        [PrivateApi]
+        public Compositor(IPlatformGraphics? gpu, bool useUiThreadForSynchronousCommits = false)
+            : this(RenderLoop.LocatorAutoInstance, gpu, useUiThreadForSynchronousCommits)
+        {
+        }
+
+        internal Compositor(IRenderLoop loop, IPlatformGraphics? gpu, bool useUiThreadForSynchronousCommits = false)
+            : this(loop, gpu, useUiThreadForSynchronousCommits, MediaContext.Instance, false, Dispatcher.UIThread)
+        {
+        }
+
+        internal Compositor(IRenderLoop loop, IPlatformGraphics? gpu,
+            bool useUiThreadForSynchronousCommits,
+            ICompositorScheduler scheduler, bool reclaimBuffersImmediately,
+            Dispatcher dispatcher)
+        {
+            Loop = loop;
+            UseUiThreadForSynchronousCommits = useUiThreadForSynchronousCommits;
+            Dispatcher = dispatcher;
+            _batchMemoryPool = new(reclaimBuffersImmediately);
+            _batchObjectPool = new(reclaimBuffersImmediately);
+            _server = new ServerCompositor(loop, gpu, _batchObjectPool, _batchMemoryPool);
+            _triggerCommitRequested = () => scheduler.CommitRequested(this);
+
+            DefaultEasing = new SplineEasing(new KeySpline(0.25, 0.1, 0.25, 1.0));
+        }
+
+        /// <summary>
+        /// Requests pending changes in the composition objects to be serialized and sent to the render thread
+        /// </summary>
+        /// <returns>A task that completes when sent changes are applied on the render thread</returns>
+        public Task RequestCommitAsync() => RequestCompositionBatchCommitAsync().Processed;
+
+        /// <summary>
+        /// Requests pending changes in the composition objects to be serialized and sent to the render thread
+        /// </summary>
+        /// <returns>A CompositionBatch object that provides batch lifetime information</returns>
+        public CompositionBatch RequestCompositionBatchCommitAsync()
+        {
+            Dispatcher.VerifyAccess();
+            if (_nextCommit == null)
+            {
+                _nextCommit = new ();
+                var pending = _pendingBatch;
+                if (pending != null)
+                    pending.Processed.ContinueWith(
+                        _ => Dispatcher.Post(_triggerCommitRequested, DispatcherPriority.Send),
+                        TaskContinuationOptions.ExecuteSynchronously);
+                else
+                    _triggerCommitRequested();
+            }
+
+            return _nextCommit;
+        }
+
+        internal CompositionBatch Commit()
+        {
+            try
+            {
+                return CommitCore();
+            }
+            finally
+            {
+                if (_invokeBeforeCommitWrite.Count > 0)
+                    RequestCommitAsync();
+                AfterCommit?.Invoke();
+            }
+        }
+        
+        CompositionBatch CommitCore()
+        {
+            Dispatcher.VerifyAccess();
+            using var noPump = NonPumpingLockHelper.Use();
+            
+            var commit = _nextCommit ??= new();
+
+            (_invokeBeforeCommitRead, _invokeBeforeCommitWrite) = (_invokeBeforeCommitWrite, _invokeBeforeCommitRead);
+            while (_invokeBeforeCommitRead.Count > 0)
+                _invokeBeforeCommitRead.Dequeue()();
+            
+            using (var writer = new BatchStreamWriter(_nextCommit.Changes, _batchMemoryPool, _batchObjectPool))
+            {
+                while(_objectSerializationQueue.TryDequeue(out var obj))
+                {
+                    var serverObject = obj.TryGetServer(this);
+                    if (serverObject != null)
+                    {
+                        writer.WriteObject(serverObject);
+                        obj.SerializeChanges(this, writer);
+#if DEBUG_COMPOSITOR_SERIALIZATION
+                        writer.Write(BatchStreamDebugMarkers.ObjectEndMagic);
+                        writer.WriteObject(BatchStreamDebugMarkers.ObjectEndMarker);
+#endif
+                    }
+                }
+                _objectSerializationHashSet.Clear();
+
+                if (_disposeOnNextBatch.Count != 0)
+                {
+                    writer.WriteObject(ServerCompositor.RenderThreadDisposeStartMarker);
+                    writer.Write(_disposeOnNextBatch.Count);
+                    foreach (var d in _disposeOnNextBatch)
+                        writer.WriteObject(d);
+                    _disposeOnNextBatch.Clear();
+                }
+
+                if (_pendingServerCompositorJobs.Count > 0)
+                {
+                    writer.WriteObject(ServerCompositor.RenderThreadJobsStartMarker);
+                    foreach (var job in _pendingServerCompositorJobs)
+                        writer.WriteObject(job);
+                    writer.WriteObject(ServerCompositor.RenderThreadJobsEndMarker);
+                }
+                _pendingServerCompositorJobs.Clear();
+            }
+            
+            _nextCommit.CommittedAt = Server.Clock.Elapsed;
+            _server.EnqueueBatch(_nextCommit);
+            
+            lock (_pendingBatchLock)
+            {
+                _pendingBatch = _nextCommit;
+                _pendingBatch.Processed.ContinueWith(t =>
+                {
+                    lock (_pendingBatchLock)
+                    {
+                        if (_pendingBatch.Processed == t)
+                            _pendingBatch = null;
+                    }
+                }, TaskContinuationOptions.ExecuteSynchronously);
+                _nextCommit = null;
+                
+                return commit;
+            }
+        }
+
+        internal void RegisterForSerialization(ICompositorSerializable compositionObject)
+        {
+            Dispatcher.VerifyAccess();
+            if(_objectSerializationHashSet.Add(compositionObject))
+                _objectSerializationQueue.Enqueue(compositionObject);
+            RequestCommitAsync();
+        }
+
+        internal void DisposeOnNextBatch(SimpleServerObject obj)
+        {
+            if (obj is IDisposable disposable && _disposeOnNextBatch.Add(disposable))
+                RequestCommitAsync();
+        }
+        
+        /// <summary>
+        /// Enqueues a callback to be called before the next scheduled commit.
+        /// If there is no scheduled commit it automatically schedules one
+        /// This is useful for updating your composition tree objects after binding
+        /// and layout passes have completed
+        /// </summary>
+        public void RequestCompositionUpdate(Action action)
+        {
+            Dispatcher.VerifyAccess();
+            _invokeBeforeCommitWrite.Enqueue(action);
+            RequestCommitAsync();
+        }
+
+        internal void PostServerJob(Action job)
+        {
+            Dispatcher.VerifyAccess();
+            _pendingServerCompositorJobs.Add(job);
+            RequestCommitAsync();
+        }
+
+        internal Task InvokeServerJobAsync(Action job) =>
+            InvokeServerJobAsync<object?>(() =>
+            {
+                job();
+                return null;
+            });
+
+        internal Task<T> InvokeServerJobAsync<T>(Func<T> job)
+        {
+            var tcs = new TaskCompletionSource<T>(TaskCreationOptions.RunContinuationsAsynchronously);
+            PostServerJob(() =>
+            {
+                try
+                {
+                    tcs.SetResult(job());
+                }
+                catch (Exception e)
+                {
+                    tcs.TrySetException(e);
+                }
+            });
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Attempts to query for a feature from the platform render interface
+        /// </summary>
+        public ValueTask<object?> TryGetRenderInterfaceFeature(Type featureType) =>
+            new(InvokeServerJobAsync(() =>
+            {
+                using (Server.RenderInterface.EnsureCurrent())
+                {
+                    return Server.RenderInterface.Value.TryGetFeature(featureType);
+                }
+            }));
+
+        public ValueTask<ICompositionGpuInterop?> TryGetCompositionGpuInterop() =>
+            new(InvokeServerJobAsync<ICompositionGpuInterop?>(() =>
+            {
+                using (Server.RenderInterface.EnsureCurrent())
+                {
+                    var feature = Server.RenderInterface.Value
+                        .TryGetFeature<IExternalObjectsRenderInterfaceContextFeature>();
+                    if (feature == null)
+                        return null;
+                    return new CompositionInterop(this, feature);
+                }
+            }));
+
+        internal bool UnitTestIsRegisteredForSerialization(ICompositorSerializable serializable) =>
+            _objectSerializationHashSet.Contains(serializable);
+    }
+    
+    internal interface ICompositorScheduler
+    {
+        void CommitRequested(Compositor compositor);
+    }
+}
---
+++ src/VisualComposition/Rendering/Composition/Server/ServerCompositor.cs
@@ -0,0 +1,260 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using Avalonia.Logging;
+using Avalonia.Platform;
+using Avalonia.Rendering.Composition.Animations;
+using Avalonia.Rendering.Composition.Expressions;
+using Avalonia.Rendering.Composition.Transport;
+using Avalonia.Threading;
+
+namespace Avalonia.Rendering.Composition.Server
+{
+    /// <summary>
+    /// Server-side counterpart of the <see cref="Compositor"/>.
+    /// 1) manages deserialization of changes received from the UI thread
+    /// 2) triggers animation ticks
+    /// 3) asks composition targets to render themselves
+    /// </summary>
+    internal partial class ServerCompositor : IRenderLoopTask
+    {
+        private readonly IRenderLoop _renderLoop;
+
+        private readonly Queue<CompositionBatch> _batches = new Queue<CompositionBatch>();
+        private readonly Queue<Action> _receivedJobQueue = new();
+        public long LastBatchId { get; private set; }
+        public Stopwatch Clock { get; } = Stopwatch.StartNew();
+        public TimeSpan ServerNow { get; private set; }
+        private readonly List<ServerCompositionTarget> _activeTargets = new();
+        private readonly HashSet<IServerClockItem> _clockItems = new();
+        private readonly List<IServerClockItem> _clockItemsToUpdate = new();
+        internal BatchStreamObjectPool<object?> BatchObjectPool;
+        internal BatchStreamMemoryPool BatchMemoryPool;
+        private readonly object _lock = new object();
+        private Thread? _safeThread;
+        private bool _uiThreadIsInsideRender;
+        public PlatformRenderInterfaceContextManager RenderInterface { get; }
+        internal static readonly object RenderThreadDisposeStartMarker = new();
+        internal static readonly object RenderThreadJobsStartMarker = new();
+        internal static readonly object RenderThreadJobsEndMarker = new();
+
+        public ServerCompositor(IRenderLoop renderLoop, IPlatformGraphics? platformGraphics,
+            BatchStreamObjectPool<object?> batchObjectPool, BatchStreamMemoryPool batchMemoryPool)
+        {
+            _renderLoop = renderLoop;
+            RenderInterface = new PlatformRenderInterfaceContextManager(platformGraphics);
+            BatchObjectPool = batchObjectPool;
+            BatchMemoryPool = batchMemoryPool;
+            _renderLoop.Add(this);
+        }
+
+        public void EnqueueBatch(CompositionBatch batch)
+        {
+            lock (_batches) 
+                _batches.Enqueue(batch);
+        }
+
+        internal void UpdateServerTime() => ServerNow = Clock.Elapsed;
+
+        readonly List<CompositionBatch> _reusableToNotifyProcessedList = new();
+        readonly List<CompositionBatch> _reusableToNotifyRenderedList = new();
+        void ApplyPendingBatches()
+        {
+            while (true)
+            {
+                CompositionBatch batch;
+                lock (_batches)
+                {
+                    if(_batches.Count == 0)
+                        break;
+                    batch = _batches.Dequeue();
+                }
+
+                using (var stream = new BatchStreamReader(batch.Changes, BatchMemoryPool, BatchObjectPool))
+                {
+                    while (!stream.IsObjectEof)
+                    {
+                        var readObject = stream.ReadObject();
+                        if (readObject == RenderThreadJobsStartMarker)
+                        {
+                            ReadServerJobs(stream);
+                            continue;
+                        }
+
+                        if (readObject == RenderThreadDisposeStartMarker)
+                        {
+                            ReadDisposeJobs(stream);
+                            continue;
+                        }
+                        
+                        var target = (SimpleServerObject)readObject!;
+                        target.DeserializeChanges(stream, batch);
+#if DEBUG_COMPOSITOR_SERIALIZATION
+                        if (stream.ReadObject() != BatchStreamDebugMarkers.ObjectEndMarker)
+                            throw new InvalidOperationException(
+                                $"Object {target.GetType()} failed to deserialize properly on object stream");
+                        if(stream.Read<Guid>() != BatchStreamDebugMarkers.ObjectEndMagic)
+                            throw new InvalidOperationException(
+                                $"Object {target.GetType()} failed to deserialize properly on data stream");
+#endif
+                    }
+                }
+
+                _reusableToNotifyProcessedList.Add(batch);
+                LastBatchId = batch.SequenceId;
+            }
+        }
+
+        void ReadServerJobs(BatchStreamReader reader)
+        {
+            object? readObject;
+            while ((readObject = reader.ReadObject()) != RenderThreadJobsEndMarker)
+                _receivedJobQueue.Enqueue((Action)readObject!);
+        }
+
+        void ReadDisposeJobs(BatchStreamReader reader)
+        {
+            var count = reader.Read<int>();
+            while (count > 0)
+            {
+                (reader.ReadObject() as IDisposable)?.Dispose();
+                count--;
+            }
+        }
+
+        void ExecuteServerJobs()
+        {
+            while(_receivedJobQueue.Count > 0)
+                try
+                {
+                    _receivedJobQueue.Dequeue()();
+                }
+                catch
+                {
+                    // Ignore
+                }
+        }
+
+        void NotifyBatchesProcessed()
+        {
+            foreach (var batch in _reusableToNotifyProcessedList) 
+                batch.NotifyProcessed();
+
+            foreach (var batch in _reusableToNotifyProcessedList)
+                _reusableToNotifyRenderedList.Add(batch);
+
+            _reusableToNotifyProcessedList.Clear();
+        }
+        
+        void NotifyBatchesRendered()
+        {
+            foreach (var batch in _reusableToNotifyRenderedList) 
+                batch.NotifyRendered();
+
+            _reusableToNotifyRenderedList.Clear();
+        }
+
+        public void Render()
+        {
+            if (Dispatcher.UIThread.CheckAccess())
+            {
+                if (_uiThreadIsInsideRender)
+                    throw new InvalidOperationException("Reentrancy is not supported");
+                _uiThreadIsInsideRender = true;
+                try
+                {
+                    using (Dispatcher.UIThread.DisableProcessing()) 
+                        RenderReentrancySafe();
+                }
+                finally
+                {
+                    _uiThreadIsInsideRender = false;
+                }
+            }
+            else
+                RenderReentrancySafe();
+        }
+        
+        private void RenderReentrancySafe()
+        {
+            lock (_lock)
+            {
+                try
+                {
+                    try
+                    {
+                        _safeThread = Thread.CurrentThread;
+                        RenderCore();
+                    }
+                    finally
+                    {
+                        NotifyBatchesRendered();
+                    }
+                }
+                finally
+                {
+                    _safeThread = null;
+                }
+            }
+        }
+        
+        private void RenderCore()
+        {
+            UpdateServerTime();
+            ApplyPendingBatches();
+            NotifyBatchesProcessed();
+            
+            foreach(var animation in _clockItems)
+                _clockItemsToUpdate.Add(animation);
+
+            foreach (var animation in _clockItemsToUpdate)
+                animation.OnTick();
+            
+            _clockItemsToUpdate.Clear();
+
+            ApplyEnqueuedRenderResourceChanges();
+            
+            try
+            {
+                RenderInterface.EnsureValidBackendContext();
+                ExecuteServerJobs();
+                foreach (var t in _activeTargets)
+                    t.Render();
+            }
+            catch (Exception e)
+            {
+                Logger.TryGet(LogEventLevel.Error, LogArea.Visual)?.Log(this, "Exception when rendering: {Error}", e);
+            }
+        }
+
+        public void AddCompositionTarget(ServerCompositionTarget target)
+        {
+            _activeTargets.Add(target);
+        }
+
+        public void RemoveCompositionTarget(ServerCompositionTarget target)
+        {
+            _activeTargets.Remove(target);
+        }
+        
+        public void AddToClock(IServerClockItem item) =>
+            _clockItems.Add(item);
+
+        public void RemoveFromClock(IServerClockItem item) =>
+            _clockItems.Remove(item);
+
+        public IRenderTarget CreateRenderTarget(IEnumerable<object> surfaces)
+        {
+            using (RenderInterface.EnsureCurrent())
+                return RenderInterface.CreateRenderTarget(surfaces);
+        }
+
+        public bool CheckAccess() => _safeThread == Thread.CurrentThread;
+        public void VerifyAccess()
+        {
+            if (!CheckAccess())
+                throw new InvalidOperationException("This object can be only accessed under compositor lock");
+        }
+    }
+}
